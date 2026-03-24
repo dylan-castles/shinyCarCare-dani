19a46eec22ac7c6ff609c64e92542a5df5dae57f
Dylan <100005958.joan23@fje.edu>
2026-03-24 13:18:22 +0100

    galeria

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const DESCUENTO_DEFAULT = 0;
 const API_PRECIOS =
   "https://script.google.com/macros/s/AKfycbzKbrHdjmGKwL42o-GfndjHsrNOT0LR_eST00c5jM9v4TISRonEiP3CRa9asdIt17YoZA/exec";
 const API_GALERIAS =
-  "https://script.google.com/macros/s/AKfycbyodLxKgTW6FZsOnMHEEaORUAcxRJYz0HkwWu29F0Fwv6szCJtJ2kF33EqbG4kx-TzW/exec";
+  "https://script.google.com/macros/s/AKfycbwDvgsEqTaF4ztyT0ddeEKmQWfL9eFnq_1Rz_Zmrh8VeHWgR9K91yTt_myH6Y8Iu5sL/exec";
 
 const GALERIA_MAP = {
   "limpieza exterior": "galeriaLimpiezaExteriorContainer",
